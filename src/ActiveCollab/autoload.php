852e8eb2398ac66f1d3396636a913be144f5295e
Ilija Studen <ilija.studen@activecollab.com>
2014-05-11 15:56:39 +0200

    Renaming files to clean up ActiveCollab namespace

--- src/ActiveCollab/autoload.php
+++ src/ActiveCollab/autoload.php
@@ -7,7 +7,7 @@
   spl_autoload_register(function($class) {
     $parts = explode('\\', $class);
 
-    if(array_shift($parts) == 'ActiveCollab') {
-      require_once __DIR__ . '/' . implode('/', $parts) . '.class.php';
+    if(array_shift($parts) == 'ActiveCollab' && array_shift($parts) == 'SDK') {
+      require_once __DIR__ . '/SDK/' . implode('/', $parts) . '.php';
     } //
   });
